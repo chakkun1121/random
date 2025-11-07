417c915d2b69305f51755a438f8c4cfec669c369
chakkun1121 <chakkun1121@users.noreply.github.com>
2025-11-07 11:47:53 +0000

    deploy: 07b6778b5db9166a5b9777a8589d238cef1b44d6

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,35 @@
 // キャッシュ名にバージョンを埋め込むためのプレースホルダー
-const CACHE_NAME = 'my-simple-pwa-cache-2025.11.04.063820';
+const CACHE_PREFIX = "random-";
+const CACHE_NAME = CACHE_PREFIX + "2025.11.07.114752";
 
 // キャッシュするファイルのリスト
 const urlsToCache = [
-  './',
-  './index.html',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  "./",
+  "./index.html",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
 ];
 
 // installイベント: ファイルをキャッシュする
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // activateイベント: 古いキャッシュを削除する
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", event => {
+  // ★ 古いキャッシュを削除するロジックを修正
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+        cacheNames.map(cacheName => {
+          // 自分のプレフィックスを持っていて、かつ現在のキャッシュ名と違うものだけを削除
+          if (cacheName.startsWith(CACHE_PREFIX) && cacheName !== CACHE_NAME) {
+            console.log("Deleting old cache:", cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -35,18 +37,16 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
-
 // fetchイベント: リクエストに応じてキャッシュから返す
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // キャッシュにあればそれを返す
-        if (response) {
-          return response;
-        }
-        //なければネットワークから取得
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      // キャッシュにあればそれを返す
+      if (response) {
+        return response;
+      }
+      //なければネットワークから取得
+      return fetch(event.request);
+    })
   );
 });
